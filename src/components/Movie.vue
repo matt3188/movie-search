e3d6abf28b046d8207cef5c6385786c4ae616b49
Matt Coleman <matt.coleman@protonmail.com>
2020-07-02 20:57:56 +0100

    Create Movie.vue

---
+++ src/components/Movie.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="movie">
+    <h3>{{ data.Title }} - {{ data.Year }}</h3>
+    <img :src="data.Poster" :alt="data.Title" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Movie",
+  props: {
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+};
+</script>
