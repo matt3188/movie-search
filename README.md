ba8014b27d4882ff9ae146720d6857089a32c164
Matt Coleman <matt.coleman@protonmail.com>
2020-07-02 20:56:17 +0100

    Added axios to the project and created a method to call the OMDB API

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # movie-search
+
 Small Vue JS app to search the OMDB API
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,5 +7,20 @@
 
 export default {
   name: "Home",
+  data: () => ({
+    apiKey: process.env.VUE_APP_API_KEY,
+    searchTerm: "",
+    results: {}
+  }),
+  methods: {
+    search(term) {
+      axios
+        .get(`https://www.omdbapi.com/?s=${term}&apikey=${this.apiKey}`)
+        .then(response => {
+          this.results = response.data;
+        })
+        .catch(error => console.log(error));
+    }
+  }
 };
 </script>
