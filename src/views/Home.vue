6fdec3699af77e18c042226cade5981a7fe98384
Matt Coleman <matt.coleman@protonmail.com>
2020-07-02 20:58:31 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="home">
     <SearchBar @search="search" />
+    <div v-for="movie in results.Search" :key="movie.id">
+      <Movie :data="movie" />
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import Movie from "@/components/Movie.vue";
 import SearchBar from "@/components/SearchBar.vue";
 
 export default {
   name: "Home",
   components: {
+    Movie,
     SearchBar
   },
   data: () => ({
